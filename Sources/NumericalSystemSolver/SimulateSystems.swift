00d6490c2cce0959f2d684e42cf4617261c64c3a
George Tait <george.tait512@gmail.com>
2023-10-09 15:56:37 +0100

    Added Extra Simulation Functionality
    Added functionality to run simulations while some condition is true, e.g for projectile motion, while the projectile is above the ground.

--- Sources/NumericalSystemSolver/SimulateSystems.swift
+++ Sources/NumericalSystemSolver/SimulateSystems.swift
@@ -38,6 +38,34 @@ func simulateTimeIndependantSystem<T: TimeIndependantSimulatableSystem> (system:
 
 }
 
+func simulateTimeIndependantSystem<T: TimeIndependantSimulatableSystem> (system: T, whileCondition: Bool, relativeTol: Double) -> SimulationDataContainer {
+    
+    var tempSystem = system
+    
+    var currentStep = 0.01
+    let initialState = tempSystem.getCurrentState()
+    var outputData = SimulationDataContainer(data: [])
+    outputData.storeSystemDataAtIteration(state: initialState)
+    
+    
+    
+    while whileCondition {
+        
+        let currentState = tempSystem.getCurrentState()
+        
+        currentStep = adaptStep(f: tempSystem.getCurrentDerivatives(currentState:placeholder:), y: currentState, x: 0, h: currentStep, relativeTol: relativeTol)
+        
+        tempSystem.updateSystemState(currentState: currentState, step: currentStep)
+
+        outputData.storeSystemDataAtIteration(state: currentState)
+    }
+    
+    tempSystem.reassignProperties(newState: initialState)
+    
+    return outputData
+
+}
+
 
 func simulateTimeDependantSystem<T: TimeDependantSimulatableSystem> (system: T, timeOfSimulation: Double, relativeTol: Double) -> SimulationDataContainer {
     
@@ -51,6 +79,36 @@ func simulateTimeDependantSystem<T: TimeDependantSimulatableSystem> (system: T, 
     var currentTime = 0.0
     
     while currentTime < timeOfSimulation {
+        
+        let currentState = tempSystem.getCurrentState()
+        
+        currentStep = adaptStep(f: tempSystem.getCurrentDerivatives(currentState:currentTime:), y: currentState, x: 0, h: currentStep, relativeTol: relativeTol)
+        
+        tempSystem.updateSystemState(currentState: currentState, currentTime: currentTime, step: currentStep)
+
+        outputData.storeSystemDataAtIteration(state: currentState)
+        
+        currentTime += currentStep
+    }
+    
+    tempSystem.reassignProperties(newState: initialState)
+    
+    return outputData
+
+}
+
+func simulateTimeDependantSystem<T: TimeDependantSimulatableSystem> (system: T, whileCondition: Bool, relativeTol: Double) -> SimulationDataContainer {
+    
+    var tempSystem = system
+    
+    var currentStep = 0.01
+    let initialState = tempSystem.getCurrentState()
+    var outputData = SimulationDataContainer(data: [])
+    outputData.storeSystemDataAtIteration(state: initialState)
+    
+    var currentTime = 0.0
+    
+    while whileCondition {
         
         let currentState = tempSystem.getCurrentState()
         
